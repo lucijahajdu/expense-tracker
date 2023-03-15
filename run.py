8f985e3904dd2e412124837fc0e1fd27f26762a9
lucijahajdu <lucija.hajdu@gmail.com>
2023-03-15 05:54:57 +0000

    Add  informations visible when section is chosen.

--- run.py
+++ run.py
@@ -7,14 +7,6 @@ expenses.append(expense2)
 def add_expense(amount, category, month):
     expense = {'amount': amount, 'category': category, 'month': month}
     exspenses.append(expense)  
-
-def view_all_transaction():
-    print('\nHere is a view of all transactions.')
-    print("------------------------------------")
-    counter = 0
-    for expense in expenses:
-    print("#", counter, " - ", expense['amount'], " - ", expense['category'])
-    counter += 1
     
 
 def print_menu():
@@ -25,9 +17,30 @@ def print_menu():
     print('----- Menu -----')
     print('1. Add a new transaction')
     print('2. View all transaction')
-    print('3. View expenses by month')
-    print('4. Remove a transaction\n')
+    print('3. Remove a transaction\n')
 
     selection = input('Please choose an option: ')
+
+    if (selection == '1'):
+        category = input('\n Enter exspense category: ')
+        amount = float(input('Enter the expense amount:'))
+        month = input('\n Enter the month of the exspense: ')
+        
+    elif (selection =='2'):
+        view_all_transaction()
+    elif (selection == '3'):
+        removeExpense()
+    else:
+        print("Invalid option. Please enter a number between 1 and 3.")
+
+def view_all_transaction():
+    print('\nHere is a view of all transactions.')
+    print("------------------------------------")
+    counter = 0
+    for expense in expenses:
+    print("#", counter, " - ", expense['amount'], " - ", expense['category']," - ", expense['month'])
+    counter += 1
+
+
 
 print_menu()

24197633334df561264c8f6b126a37c363d62ea1
lucijahajdu <lucija.hajdu@gmail.com>
2023-03-15 18:56:08 +0000

    Add section to remove transactin

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ def print_menu():
     """
     Menu area to choose an option
     """
+
     print('\nWelcome to Expense Tracker.\n')
     print('----- Menu -----')
     print('1. Add a new transaction')
@@ -20,16 +21,17 @@ def print_menu():
     print('3. Remove a transaction\n')
 
     selection = input('Please choose an option: ')
-
+   
+    
     if (selection == '1'):
         category = input('\n Enter exspense category: ')
-        amount = float(input('Enter the expense amount:'))
+        amount = float(input('\nEnter the expense amount:'))
         month = input('\n Enter the month of the exspense: ')
         
     elif (selection =='2'):
         view_all_transaction()
     elif (selection == '3'):
-        removeExpense()
+        remove_transaction()
     else:
         print("Invalid option. Please enter a number between 1 and 3.")
 
@@ -38,8 +40,24 @@ def view_all_transaction():
     print("------------------------------------")
     counter = 0
     for expense in expenses:
-    print("#", counter, " - ", expense['amount'], " - ", expense['category']," - ", expense['month'])
-    counter += 1
+        print(counter, " - ", expense['amount'], " - ", expense['category']," - ", expense['month'])
+        counter += 1
+    print('\n\n')
+
+
+def remove_transaction():
+    while True:
+        view_all_transaction()
+    
+        try:
+            remove = int(input('Remove transaction: '))
+            del expenses[remove]
+            break
+        except:
+            print('Invalid input. Please try again.')
+        return False
+    
+
 
 
 
